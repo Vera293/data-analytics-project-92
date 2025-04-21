0f096779932f181c9dd1a43f7200c6e51311aa73
Vera293 <fireses@ya.ru>
2025-04-21 20:58:59 +0300

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -1,50 +1,48 @@
-SELECT 
-    CONCAT(e.first_name, ' ', e.last_name) AS seller,  -- Полное имя продавца
-    COUNT(s.sales_id) AS operations,                   -- Количество операций (продаж)
-    SUM(p.price * s.quantity) AS income                -- Общий доход (сумма стоимости всех продуктов)
-FROM employees e                                      -- Таблица сотрудников
-JOIN sales s ON e.employee_id = s.sales_person_id      -- Соединяем сотрудников с продажами по employee_id
-JOIN products p ON s.product_id = p.product_id        -- Соединяем товары с продажами по product_id
-GROUP BY e.employee_id, e.first_name, e.last_name     -- Группируем по сотрудникам
-ORDER BY income DESC                                  -- Сортируем по доходу в обратном порядке (от большего к меньшему)
-LIMIT 10;                                             -- Ограничиваемся первыми десятью результатами
-
-WITH AverageSales AS (                                     -- Вспомогательное представление (CTE)
+WITH AgeCategories AS (
     SELECT
-        s.sales_person_id,                                -- id продавца
-        e.first_name || ' ' || e.last_name AS seller,     -- Полное имя продавца
-        AVG(s.quantity * p.price) AS avg_sale             -- Средняя стоимость одной продажи
-    FROM sales s                                          -- Таблица продаж
-    JOIN employees e ON s.sales_person_id = e.employee_id -- Связываем сотрудников с продажами
-    JOIN products p ON s.product_id = p.product_id       -- Связываем товары с продажами
-    GROUP BY s.sales_person_id, e.first_name, e.last_name -- Группируем по продавцам
-),
-OverallAverage AS (                                        -- Второе вспомогательное представление
-    SELECT AVG(avg_sale) AS overall_avg                   -- Общая средняя стоимость продажи
-    FROM AverageSales                                      -- Берём среднее из предыдущей CTE
+        CASE
+            WHEN age BETWEEN 16 AND 25 THEN '16-25'
+            WHEN age BETWEEN 26 AND 40 THEN '26-40'
+            ELSE '40+'
+        END AS age_category,-- Создаем новое поле "age_category" на основе возраста
+        COUNT(*) AS age_count-- Считаем количество записей в каждой категории
+    FROM customers
+    GROUP BY age_category-- Группируем результаты по категориям возраста
+    ORDER BY age_category-- Сортируем результаты по возрастанию категорий
 )
 SELECT
-    seller,                                               -- Имя продавца
-    ROUND(avg_sale) AS average_income                      -- Округлённая средняя выручка
-FROM AverageSales                                         -- База данных из предыдущего CTE
-WHERE avg_sale < (SELECT overall_avg FROM OverallAverage)  -- Только продавцы с меньшей средней выручкой
-ORDER BY average_income ASC;                               -- Сортируем по средней выручке (от наименьшей к наибольшей)
+    age_category, -- Выбираем категорию возраста
+    age_count-- Выбираем количество покупателей в каждой категории
+FROM AgeCategories;-- Берем данные из CTE (Common Table Expression) "AgeCategories"
 
+SELECT 
+    TO_CHAR(sale_date, 'YYYY-MM') AS selling_month, -- Преобразует дату в формат YYYY-MM
+    COUNT(DISTINCT customer_id) AS total_customers, -- Считает уникальных покупателей в каждом месяце
+    SUM(quantity) AS income -- СУММИРУЕТ КОЛИЧЕСТВО ТОВАРОВ
+FROM 
+    sales
+GROUP BY 
+    selling_month-- Группируем по месяцам
+ORDER BY 
+    selling_month ASC;-- Сортируем по месяцам в возрастающем порядке
 
+WITH FirstPurchase AS (
+    SELECT
+        customer_id,-- Выбираем ID покупателя
+        MIN(sale_date) AS first_purchase_date-- Находим минимальную (самую раннюю) дату продажи для каждого покупателя
+    FROM sales
+    JOIN products ON sales.product_id = products.product_id -- Делаем JOIN с таблицей products, используя product_id
+    WHERE price = 0-- Фильтруем продажи, где цена товара равна 0 (акционные товары)
+    GROUP BY customer_id-- Группируем результаты по customer_id, чтобы найти минимальную дату для каждого покупателя
+)
 SELECT
-    CONCAT(TRIM(e.first_name), ' ', TRIM(e.last_name)) AS seller,  -- Полное имя продавца
-    CASE EXTRACT(isodow FROM s.sale_date)                       -- Определение дня недели
-        WHEN 1 THEN 'monday'                                   -- Номер дня недели по стандартам ISO
-        WHEN 2 THEN 'tuesday'
-        WHEN 3 THEN 'wednesday'
-        WHEN 4 THEN 'thursday'
-        WHEN 5 THEN 'friday'
-        WHEN 6 THEN 'saturday'
-        ELSE 'sunday'
-    END AS day_of_week,                                        -- Название дня недели
-    FLOOR(SUM(p.price * s.quantity)) AS income                  -- Суммарный доход за день
-FROM sales s                                                   -- Таблица продаж
-JOIN employees e ON s.sales_person_id = e.employee_id          -- Соединяем сотрудников с продажами
-JOIN products p ON s.product_id = p.product_id                -- Соединяем товары с продажами
-GROUP BY e.first_name, e.last_name, EXTRACT(isodow FROM s.sale_date)  -- Группируем по продавцам и дням недели
-ORDER BY EXTRACT(isodow FROM s.sale_date), seller;             -- Сортируем по дням недели и продавцам
+    c.first_name || ' ' || c.last_name AS customer,-- Конкатенируем имя и фамилию покупателя из таблицы customers
+    fp.first_purchase_date AS sale_date,-- Выбираем дату первой покупки из CTE FirstPurchase
+    e.first_name || ' ' || e.last_name AS seller-- Конкатенируем имя и фамилию продавца из таблицы employees
+FROM FirstPurchase fp
+JOIN customers c ON fp.customer_id = c.customer_id -- Соединяем FirstPurchase с customers по customer_id
+JOIN sales s ON fp.customer_id = s.customer_id AND fp.first_purchase_date = s.sale_date-- Соединяем с sales, используя customer_id и дату первой покупки
+JOIN employees e ON s.sales_person_id = e.employee_id-- Соединяем с employees по sales_person_id
+ORDER BY c.customer_id;-- Сортируем результат по customer_id
+
+
